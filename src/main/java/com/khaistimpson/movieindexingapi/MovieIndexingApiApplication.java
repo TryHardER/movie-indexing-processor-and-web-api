5981bbd335a3df9e971ce489ee41251ac15d0234
Khai <khai_stimpson@hotmail.com>
2018-02-14 15:12:05 +1000

    Initial Commit

---
+++ src/main/java/com/khaistimpson/movieindexingapi/MovieIndexingApiApplication.java
@@ -0,0 +1,12 @@
+package com.khaistimpson.movieindexingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieIndexingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieIndexingApiApplication.class, args);
+	}
+}
